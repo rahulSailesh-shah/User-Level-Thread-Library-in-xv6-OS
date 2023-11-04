0c174b30fb2b890e81fff4b792602756eedbdb44
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2023-11-04 14:32:59 -0700

    user level thread library completed, system call not implemented

---
+++ xv6-riscv/user/ulthread.c
@@ -0,0 +1,152 @@
+/* CSE 536: User-Level Threading Library */
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+#include "user/ulthread.h"
+
+/* Standard definitions */
+#include <stdbool.h>
+#include <stddef.h>
+
+// user_level_thread
+
+int gThreadID = 0;
+int threadsDestroyed = 0;
+struct user_level_thread *current_running_thread;
+struct user_level_thread uthreads[MAXULTHREADS];
+struct user_level_thread scheduling_thread;
+
+/* Get thread ID*/
+int get_current_tid()
+{
+    return current_running_thread->thread_id;
+}
+
+/* Thread initialization */
+void ulthread_init(int schedalgo)
+{
+    current_running_thread = &scheduling_thread;
+    current_running_thread->thread_id = gThreadID;
+    current_running_thread->algo = schedalgo;
+}
+
+/* Thread creation */
+bool ulthread_create(uint64 start, uint64 stack, uint64 args[], int priority)
+{
+
+    struct user_level_thread *uthread;
+    for (uthread = uthreads; uthread < &uthreads[MAXULTHREADS]; uthread++)
+    {
+        if (uthread->state == FREE)
+            break;
+    }
+
+    uthread->state = RUNNABLE;
+    uthread->thread_id = ++gThreadID;
+    memset(&uthread->context, 0, sizeof uthread->context);
+    uthread->context.args = (int)args[0];
+    uthread->context.ra = start;
+    uthread->context.sp = stack;
+    uthread->priority = priority;
+
+    /* Please add thread-id instead of '0' here. */
+    printf("[*] ultcreate(tid: %d, ra: %p, sp: %p)\n", uthread->thread_id, start, stack);
+
+    return false;
+}
+
+void ulthread_schedule_roundrobin()
+{
+    struct user_level_thread *uthread;
+
+    uthread = current_running_thread + 1;
+    while (threadsDestroyed < gThreadID)
+    {
+        if (uthread > &uthreads[MAXULTHREADS])
+            uthread = uthreads;
+        if (uthread->state == RUNNABLE || uthread->state == YIELD)
+        {
+            current_running_thread = uthread;
+            current_running_thread->state = RUNNABLE;
+            printf("[*] ultschedule (next tid: %d)\n", current_running_thread->thread_id);
+            struct user_level_thread *scheduler;
+            scheduler = &scheduling_thread;
+            ulthread_context_switch(&scheduler->context, &uthread->context);
+        }
+        uthread = uthread + 1;
+    }
+}
+
+void ulthread_schedule_priority()
+{
+    struct user_level_thread *next_thread = 0;
+    struct user_level_thread *uthread, *t1, *t2;
+    uthread = current_running_thread + 1;
+    t2 = current_running_thread + 1;
+    while (threadsDestroyed < gThreadID)
+    {
+        if (uthread > &uthreads[MAXULTHREADS])
+            uthread = uthreads;
+        if ((uthread->state == RUNNABLE || uthread->state == YIELD))
+        {
+            for (t1 = uthreads; t1 < &uthreads[MAXULTHREADS]; t1++)
+            {
+                if ((t1->priority > uthread->priority) && (t1->state == RUNNABLE || t1->state == YIELD) && t1 != current_running_thread)
+                {
+                    uthread = t1;
+                }
+            }
+            current_running_thread = uthread;
+            current_running_thread->state = RUNNABLE;
+            printf("[*] ultschedule (next tid: %d)\n", current_running_thread->thread_id);
+            struct user_level_thread *scheduler;
+            scheduler = &scheduling_thread;
+            ulthread_context_switch(&scheduler->context, &uthread->context);
+        }
+        t2 = t2 + 1;
+        uthread = t2;
+    }
+}
+
+void ulthread_schedule_fcfs()
+{
+}
+
+/* Thread scheduler */
+void ulthread_schedule(void)
+{
+    if (current_running_thread->algo == ROUNDROBIN)
+    {
+        ulthread_schedule_roundrobin();
+    }
+    else if (current_running_thread->algo == PRIORITY)
+    {
+        ulthread_schedule_priority();
+    }
+    else if (current_running_thread->algo == FCFS)
+    {
+        ulthread_schedule_fcfs();
+    }
+}
+
+/* Yield CPU time to some other thread. */
+void ulthread_yield(void)
+{
+    printf("[*] ultyield(tid: %d)\n", current_running_thread->thread_id);
+    current_running_thread->state = YIELD;
+    struct user_level_thread *scheduler;
+    scheduler = &scheduling_thread;
+    ulthread_context_switch(&current_running_thread->context, &scheduler->context);
+}
+
+/* Destroy thread */
+void ulthread_destroy(void)
+{
+    threadsDestroyed++;
+    printf("[*] ultdestroy(tid: %d)\n", current_running_thread->thread_id);
+    current_running_thread->state = FREE;
+    struct user_level_thread *scheduler;
+    scheduler = &scheduling_thread;
+    ulthread_context_switch(&current_running_thread->context, &scheduler->context);
+}
---
+++ xv6-riscv/user/ulthread.h
@@ -0,0 +1,53 @@
+#ifndef __UTHREAD_H__
+#define __UTHREAD_H__
+
+#include <stdbool.h>
+
+#define MAXULTHREADS 100
+
+enum ulthread_state
+{
+  FREE,
+  RUNNABLE,
+  YIELD,
+};
+
+enum ulthread_scheduling_algorithm
+{
+  ROUNDROBIN,
+  PRIORITY,
+  FCFS, // first-come-first serve
+};
+
+struct context
+{
+  uint64 ra;
+  uint64 sp;
+
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+  int args;
+};
+
+struct user_level_thread
+{
+  enum ulthread_state state;
+  int thread_id;
+  struct context context;
+  int priority;
+  enum ulthread_scheduling_algorithm algo;
+};
+
+void ulthread_context_switch(struct ctx *, struct ctx *);
+
+#endif
